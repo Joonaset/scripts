f3f55029b74e29d28afb70f7c3cec97ace537d0e
Joonas Taivalmaa <joonas.taivalmaa@metropolia.fi>
2020-06-04 21:07:53 +0300

    Added bitwarden.sh, a script to use with qutebrowser

---
+++ bitwarden.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# TODO checks and options for external programs
+URL=$(echo "$QUTE_URL" | awk -F[/:] '{print $4}')
+notify-send "Fetching password for $URL"
+
+mpw=$(dmenupass "Master Password")
+session=$(bw unlock "$mpw" --raw) || notify-send "Wrong password"
+pass=$(bw get password "$URL" --session "$session")
+echo "$pass" | xsel -b -t 10000 || exit 1
+
+notify-send -t 10000 "Password Copied to clipboard"
