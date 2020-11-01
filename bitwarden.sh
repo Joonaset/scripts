c296a9fc222fd196b0a717ea346d6b7026f9799b
Joonas Taivalmaa <joonas.taivalmaa@metropolia.fi>
2020-11-01 21:00:37 +0200

    bitwarden exits nicely when wrong password given,

--- bitwarden.sh
+++ bitwarden.sh
@@ -4,8 +4,8 @@ URL=$(echo "$QUTE_URL" | awk -F[/:] '{print $4}')
 notify-send "Fetching password for $URL"
 
 mpw=$(dmenupass "Master Password")
-session=$(bw unlock "$mpw" --raw) || notify-send "Wrong password"
-pass=$(bw get password "$URL" --session "$session")
+session=$(bw unlock "$mpw" --raw) || notify-send "Wrong password" && exit 0
+pass=$(bw get password "$URL" --session "$session") &&
 echo "$pass" | xsel -b -t 10000 || exit 1
 
 notify-send -t 10000 "Password Copied to clipboard"
