9da94f5ebb6937b78cab30d9271775d6e3c87ea7
Joonas Taivalmaa <joonas.taivalmaa@metropolia.fi>
2020-11-01 22:21:31 +0200

    reverting previous changes

--- bitwarden.sh
+++ bitwarden.sh
@@ -4,7 +4,7 @@ URL=$(echo "$QUTE_URL" | awk -F[/:] '{print $4}')
 notify-send "Fetching password for $URL"
 
 mpw=$(dmenupass "Master Password")
-session=$(bw unlock "$mpw" --raw) || notify-send "Wrong password" && exit 0
+session=$(bw unlock "$mpw" --raw) || notify-send "Wrong password"
 pass=$(bw get password "$URL" --session "$session") &&
 echo "$pass" | xsel -b -t 10000 || exit 1
 
